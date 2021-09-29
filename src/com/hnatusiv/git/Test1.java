9b4d3b0b3b7855a169d9687a16a78466ec19b1fa
hnatusiv <hnatusivit@ukr.net>
2021-09-29 13:49:03 +0300

    Add class Test1 Add  constructor and get ,set

--- src/com/hnatusiv/git/Test1.java
+++ src/com/hnatusiv/git/Test1.java
@@ -2,4 +2,16 @@ package com.hnatusiv.git;
 
 public class Test1 {
     int id;
+
+    public Test1(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
